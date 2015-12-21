bd06c3584e9e91e6a8117bf61984f3fcd59542bb
Mikkel Tschentscher <hello@mikkeltschentscher.dk>
2015-12-21 17:43:13 +0100

    Update module-sponsors.php

--- includes/module-sponsors.php
+++ includes/module-sponsors.php
@@ -16,7 +16,7 @@
 			<div class="row">
 				<div class="col-xs-12 col-sm-3 col-md-3">
 					<p class="h4">Titelsponsor</p>
-					<img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="http://www.sportscamp.dk/wp-content/themes/sportscamp/assets/img/head-sponsor.jpg"  alt="SportsCamp titelsponser" class="img-responsive">
+					<img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="http://www.sportscamp.dk/wp-content/themes/sportscamp.dk/assets/img/head-sponsor.jpg"  alt="SportsCamp titelsponser" class="img-responsive">
 				</div>
 				<div class="col-xs-12 col-sm-9 col-md-9">
 					<div class="row">
@@ -24,13 +24,13 @@
 							<p class="h4">Hovedsponsorer</p>
 						</div>
 						<div class="col-xs-4">
-							<img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="http://www.sportscamp.dk/wp-content/themes/sportscamp/assets/img/4.png" alt="SportsCamp hovedsponser" class="img-responsive">
+							<img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="http://www.sportscamp.dk/wp-content/themes/sportscamp.dk/assets/img/4.png" alt="SportsCamp hovedsponser" class="img-responsive">
 						</div>
 						<div class="col-xs-4">
-							<img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="http://www.sportscamp.dk/wp-content/themes/sportscamp/assets/img/picture-sponsor.jpg" alt="SportsCamp hovedsponser" class="img-responsive">
+							<img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="http://www.sportscamp.dk/wp-content/themes/sportscamp.dk/assets/img/picture-sponsor.jpg" alt="SportsCamp hovedsponser" class="img-responsive">
 						</div>
 						<div class="col-xs-4">
-							<img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="http://www.sportscamp.dk/wp-content/themes/sportscamp/assets/img/spy-sponsor.jpg" alt="SportsCamp hovedsponser" class="img-responsive">
+							<img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="http://www.sportscamp.dk/wp-content/themes/sportscamp.dk/assets/img/spy-sponsor.jpg" alt="SportsCamp hovedsponser" class="img-responsive">
 						</div>
 					</div>
 				</div>
@@ -43,7 +43,7 @@
 			<div class="row">
 				<div class="col-xs-12 col-sm-3 col-md-3">
 					<p class="h4">Titelsponsor</p>
-					<img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="http://www.sportscamp.dk/wp-content/themes/sportscamp/assets/img/3.png"  alt="SportsCamp titelsponser" class="img-responsive">
+					<img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="http://www.sportscamp.dk/wp-content/themes/sportscamp.dk/assets/img/3.png"  alt="SportsCamp titelsponser" class="img-responsive">
 				</div>
 				<div class="col-xs-12 col-sm-9 col-md-9">
 					<div class="row">
@@ -51,13 +51,13 @@
 							<p class="h4">Hovedsponsorer</p>
 						</div>
 						<div class="col-xs-4">
-							<img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="http://www.sportscamp.dk/wp-content/themes/sportscamp/assets/img/2.png" alt="SportsCamp hovedsponser" class="img-responsive">
+							<img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="http://www.sportscamp.dk/wp-content/themes/sportscamp.dk/assets/img/2.png" alt="SportsCamp hovedsponser" class="img-responsive">
 						</div>
 						<div class="col-xs-4">
-							<img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="http://www.sportscamp.dk/wp-content/themes/sportscamp/assets/img/1.png" alt="SportsCamp hovedsponser" class="img-responsive">
+							<img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="http://www.sportscamp.dk/wp-content/themes/sportscamp.dk/assets/img/1.png" alt="SportsCamp hovedsponser" class="img-responsive">
 						</div>
 						<div class="col-xs-4">
-							<img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="http://www.sportscamp.dk/wp-content/themes/sportscamp/assets/img/4.png" alt="SportsCamp hovedsponser" class="img-responsive">
+							<img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="http://www.sportscamp.dk/wp-content/themes/sportscamp.dk/assets/img/4.png" alt="SportsCamp hovedsponser" class="img-responsive">
 						</div>
 					</div>
 				</div>
