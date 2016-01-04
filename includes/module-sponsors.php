d85de71bb1275433deb84beafe09f588fc17c3da
Mikkel Tschentscher <hello@mikkeltschentscher.dk>
2016-01-04 10:25:23 +0100

    Update module-sponsors.php

--- includes/module-sponsors.php
+++ includes/module-sponsors.php
@@ -41,14 +41,13 @@
 	<div class="sponsers hidden-xs hidden-print">
 		<div class="container">
 			<div class="row">
-				<div class="col-xs-12 col-sm-3 col-md-3">
-					<p class="h4">Titelsponsor</p>
-					<img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="http://www.sportscamp.dk/wp-content/themes/sportscamp.dk/assets/img/3.png"  alt="SportsCamp titelsponser" class="img-responsive">
-				</div>
-				<div class="col-xs-12 col-sm-9 col-md-9">
+				<div class="col-xs-12 col-sm-12 col-md-12">
 					<div class="row">
 						<div class="col-md-12">
 							<p class="h4">Hovedsponsorer</p>
+						</div>
+						<div class="col-xs-4">
+							<img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="http://www.sportscamp.dk/wp-content/themes/sportscamp.dk/assets/img/3.png"  alt="SportsCamp hovedsponser" class="img-responsive">
 						</div>
 						<div class="col-xs-4">
 							<img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="http://www.sportscamp.dk/wp-content/themes/sportscamp.dk/assets/img/2.png" alt="SportsCamp hovedsponser" class="img-responsive">
